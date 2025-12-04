f975278b5138bc72ca184d291f2df55445a60a41
BrynnL <brynn.landry2@student.sl.on.ca>
2025-12-04 12:11:17 -0500

    tis done

--- GameDevGroupPro/Assets/Scripts/Collectibles/CollectibleCarrot.cs
+++ GameDevGroupPro/Assets/Scripts/Collectibles/CollectibleCarrot.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using TMPro;
 
 public class CollectibleCarrot : MonoBehaviour
 {
+    private TextMeshProUGUI pointText;
 
+    private void Start()
+    {
+        pointText = GameObject.FindWithTag("pointText").GetComponent<TextMeshProUGUI>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
+
         if (collision.gameObject.tag == "Player")
         {
             Bunny player = collision.gameObject.GetComponent<Bunny>();
@@ -14,12 +22,14 @@ public class CollectibleCarrot : MonoBehaviour
                 player.carrot += 1;
                 Destroy(gameObject);
                 SoundManager.Instance.PlaySFX("CARROT");
+                pointText.text = player.carrot.ToString();
             }
             if (gameObject.tag == "Gem")
             {
                 player.carrot += 5;
                 Destroy(gameObject);
                 SoundManager.Instance.PlaySFX("GEM");
+                pointText.text = player.carrot.ToString();
             }
 
         }
