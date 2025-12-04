25a151b679e8b55e9aaa428b89d2adc4561548da
Kelly Mahoney <kelly.mahoney@student.sl.on.ca>
2025-12-04 14:25:03 -0500

    player prefs
    added the level lock system, and the playerprefs

---
+++ GameDevGroupPro/Assets/Scripts/LevelButton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelButton : MonoBehaviour
+{
+    public int level; 
+    void Start()
+    {
+        Button btn = GetComponent<Button>();
+
+        if(PlayerPrefs.GetInt("LevelReached") < level)
+        {
+            btn.interactable = false; 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
